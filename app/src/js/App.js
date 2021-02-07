053203880bfd825f5298e7e1b3a3decc7a48e5ab
ryan10145 <chang.ryan10145@gmail.com>
2021-02-06 23:00:48 -0500

    Add Alert System

--- app/src/js/App.js
+++ app/src/js/App.js
@@ -5,6 +5,8 @@ import Login from './Login'
 
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Button} from "react-bootstrap";
+import {AlertList} from "react-bs-notifier";
 
 class App extends React.Component {
     
@@ -12,15 +14,43 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            loggedIn: false
+            loggedIn: false,
+            alerts: []
         }
 
         this.setLoggedIn = this.setLoggedIn.bind(this);
+        this.onDismissAlert = this.onDismissAlert.bind(this);
     }
 
     setLoggedIn(loggedIn) {
         this.setState({
             loggedIn: loggedIn
+        });
+    }
+
+    addAlert(type, headline, message) {
+        let alerts = this.state.alerts.slice();
+        alerts.push({
+            id: new Date().getTime(),
+            type: type,
+            headline: headline,
+            message: message
+        });
+
+        this.setState({
+            alerts: alerts
+        });
+    }
+
+    onDismissAlert(alert) {
+        let i = this.state.alerts.indexOf(alert);
+        if(i < 0) return;
+
+        let alerts = this.state.alerts.slice();
+        alerts.splice(i, 1);
+
+        this.setState({
+            alerts: alerts
         });
     }
     
@@ -30,20 +60,31 @@ class App extends React.Component {
                 <AppNavbar
                     loggedIn={this.state.loggedIn}
                     setLoggedIn={this.setLoggedIn}
+                    addAlert={this.addAlert}
+                />
+                <AlertList
+                    position="top-right"
+                    alerts={this.state.alerts}
+                    timeout={10000}
+                    dismissTitle="Close Alert"
+                    onDismiss={this.onDismissAlert}
                 />
                 <Route path="/" exact>
                     <Main
                         loggedIn={this.state.loggedIn}
+                        addAlert={this.addAlert}
                     />
                 </Route>
                 <Route path="/register">
                     <Register
                         setLoggedIn={this.setLoggedIn}
+                        addAlert={this.addAlert}
                     />
                 </Route>
                 <Route path="/login">
                     <Login
                         setLoggedIn={this.setLoggedIn}
+                        addAlert={this.addAlert}
                     />
                 </Route>
             </Router>
