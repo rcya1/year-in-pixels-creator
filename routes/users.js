c9b34282c74bec34019a84378f8316f6deaf0677
ryan10145 <chang.ryan10145@gmail.com>
2021-06-17 12:48:11 -0400

    Add Nodemailer Functionality

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,18 @@ let SettingsSchema = require('../models/settings.model');
 const passport = require('passport');
 const asyncHandler = require('express-async-handler');
 let {log, Status} = require('./route_logger');
-
+const nodemailer = require('nodemailer');
 const { v4: uuidv4 } = require('uuid');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const transport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.VERIFICATION_EMAIL,
+        pass: process.env.VERIFICATION_PASSWORD
+    }
+});
 
 /**
  * GET
@@ -105,15 +115,15 @@ router.route('/change-email/:email').post(asyncHandler(async(req, res) => {
     let user = await UserSchema.findById(req.user._id);
     let newEmail = req.params.email;
 
-    if(user.email === newEmail) {
-        log(res, Status.ERROR, "Email address already associated with this account");
-        return;
+    if(user.email !== newEmail) {
+        let exists = await UserSchema.exists({email: newEmail});
+        if(exists) {
+            log(res, Status.ERROR, "Email address associated with different account");
+            return;
+        }
     }
-
-    let exists = await UserSchema.exists({email: newEmail});
-    if(exists) {
-        log(res, Status.ERROR, "Email address associated with different account");
-        return;
+    else if(user.emailVerified) {
+        log(res, Status.ERROR, "Email address already verified");
     }
 
     user.email = newEmail;
@@ -121,8 +131,16 @@ router.route('/change-email/:email').post(asyncHandler(async(req, res) => {
     user.emailVerificationToken = uuidv4();
     user.emailVerificationTokenDate = Date.now();
     await user.save();
+
+    await transport.sendMail({
+        from: "Year in Pixels",
+        to: user.email,
+        subject: "Year in Pixels Creator Email Verification",
+        html: "Press <a href=http://localhost:3000/verify/" + user.username + "/" + user.emailVerificationToken + 
+            ">here</a> within 24 hours to verify your email. Thank you!"
+    });
     
-    log(res, Status.SUCCESS, "Added email.");
+    log(res, Status.SUCCESS, "Added email and sent verification email.");
 }));
 
 /**
@@ -148,7 +166,7 @@ router.route('/change-email/').delete(asyncHandler(async(req, res) => {
 
 /**
  * POST
- * Verifies the email address with a verification token sent to email
+ * Verifies the email address with a verification token sent to email with a 24 hour limit
  */
 router.route('/verify-email/:user/:token').post(asyncHandler(async(req, res) => {
     let user = await UserSchema.findOne({username: req.params.user});
